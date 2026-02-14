dbdcb62f5f4d718b0a7171bbbc306afeb40a3802
sanket jejurkar <sanketjejurkar@gmail.com>
2026-02-15 04:40:24 +0530

    added first basic program

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
 print("Hello world")
+
+# take input from user as number dynamically and then print message as "I'm joining product company"
+n = int(input("Please Enter the Number:"))
+for i in range(n):
+    print("I'm joining product company")
+
